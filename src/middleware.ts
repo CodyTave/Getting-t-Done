af018ec4d4ce731f6a65887fe5bb6f1b0245348b
oussama dahir <dahiroussama@gmail.com>
2024-05-16 18:19:02 +0100

    16/5

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,48 +1,43 @@
-// import { NextResponse, NextRequest } from "next/server";
-// import { jwtVerify } from "jose";
+import { NextResponse, NextRequest } from "next/server";
+import { jwtVerify } from "jose";
 
-// function verifyToken(token: string, secret: string) {
-//   try {
-//     return jwtVerify(token, new TextEncoder().encode(secret));
-//   } catch (error) {
-//     console.log("error", error);
+function verifyToken(token: string, secret: string) {
+  try {
+    return jwtVerify(token, new TextEncoder().encode(secret));
+  } catch (error) {
+    console.log("error", error);
 
-//     return null;
-//   }
-// }
+    return null;
+  }
+}
 
-// function handleAuthentication(request: NextRequest) {
-//   const token = request.cookies.get("session");
-//   if (!token) return false;
+function handleAuthentication(request: NextRequest) {
+  const token = request.cookies.get("session");
+  if (!token) return false;
 
-//   const decoded = verifyToken(token.value, process.env.JWT_SECRET!);
-//   return !!decoded;
-// }
+  const decoded = verifyToken(token.value, process.env.JWT_SECRET!);
+  return !!decoded;
+}
 
-// export async function middleware(request: NextRequest) {
-//   const { pathname } = request.nextUrl;
-//   // const excludedPaths = ["/api/auth/login"];
-//   const assetPattern = /^\/(_next\/static|static|public)\//;
-
-//   if (
-//     assetPattern.test(pathname) ||
-//     // excludedPaths.includes(pathname) ||
-//     pathname.startsWith("/api/")
-//   ) {
-//     return NextResponse.next();
-//   }
-
-//   const isAuthenticated = handleAuthentication(request);
-
-//   if (pathname === "/login" || pathname === "/singup") {
-//     return isAuthenticated
-//       ? NextResponse.redirect(new URL("/", request.url))
-//       : NextResponse.next();
-//   }
-
-//   if (!isAuthenticated) {
-//     return NextResponse.redirect(new URL("/login", request.url));
-//   }
-
-//   return NextResponse.next();
-// }
+export async function middleware(request: NextRequest) {
+  // const { pathname } = request.nextUrl;
+  // // const excludedPaths = ["/api/auth/login"];
+  // const assetPattern = /^\/(_next\/static|static|public)\//;
+  // if (
+  //   assetPattern.test(pathname) ||
+  //   // excludedPaths.includes(pathname) ||
+  //   pathname.startsWith("/api/")
+  // ) {
+  //   return NextResponse.next();
+  // }
+  // const isAuthenticated = handleAuthentication(request);
+  // if (pathname === "/login" || pathname === "/singup") {
+  //   return isAuthenticated
+  //     ? NextResponse.redirect(new URL("/", request.url))
+  //     : NextResponse.next();
+  // }
+  // if (!isAuthenticated) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
+  // return NextResponse.next();
+}
